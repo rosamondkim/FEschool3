f89279148d0e17ac27c954bdeb09c053f53557ef
rosamondkim <itzmems@naver.com>
2022-11-25 22:42:39 +0900

    원범님특강

--- 221121_리액트/034/my-app/src/App copy 4.js
+++ 221121_리액트/034/my-app/src/App copy 4.js
@@ -1,68 +1,69 @@
-// 이거 복붙 다시하기
-
-import React ,{useRef, useState} from 'react'
+import React, { useRef, useState } from 'react'
 
 export default function App() {
   const inputName = useRef(null)
   const inputId = useRef(null)
-  const [userInfo,setUserInfo] = useState([]) //빈 어레이로 초기화
+  const [userInfo, setUserInfo] = useState([])
+  const [name, setName] = useState('')
+  const [id, setId] = useState('')
 
-  const [name,setName] = useState('')
-  const [id,setId] = useState('')
+  function handleInputName(e){
+    console.log(e)
+    setName(e.target.value)
+    console.log('렌더링 - 1')
+  }
 
-  function handleInputName(){
-    setName(e)
-    setName(e.target.value)
-    console.log('랜더링-1')
+  function handleInputId(e){
+    console.log(e)
+    setId(e.target.value)
+    console.log('렌더링 - 2')
   }
-  function handleInputId(){
-    setName(e)
-    setName(e.target.value)
-    console.log('랜더링-2')
+
+  function handleSubmit(e){
+    e.preventDefault()
+    // userInfo.push({});
+    const newInfo = [...userInfo, {name, id}]
+    inputName.current.value = ''
+    inputId.current.value = ''
+    inputName.current.focus()
+    setUserInfo(newInfo)
+    console.log('렌더링 - 3')
   }
-  function handleSubmit(){
-    
-    setName(e)
-    setName(e.target.value)
-    console.log('랜더링-3')
-  }
-  function getNum(){
-    setName(e)
-    setName(e.target.value)
-    console.log('랜더링-4')
+
+  // 모든 렌더링에 함께 렌더링되는 이슈가 있습니다.
+  function getNum(li){
+    console.log('렌더링!')
+    return li.length
   }
 
   return (
     <>
-    <form>
-    <input type="text" 
-    placeholder='이름입력하세요'
-    onChange={handleInputName}
-    ref={inputName}
-    />
-    <input type="text" 
-    placeholder='이름입력하세요'
-    onChange={handleInputId}
-    ref={inputId}
-    />
-    <button type='submit' onClick={handleSubmit}>버튼</button>
-    </form> 
-    <ul>
-      {
-          // userInfo 어레이니까 이거 순회를 돌면서 ...
-          userInfo.map((value,index)=> (
-              // 중괄호 있으면 구문이 들어가고 없으면 바로 리턴되는것 들어가야함
+      <form>
+        <input 
+          type='text'
+          placeholder='이름을 입력하세요'
+          onChange={handleInputName}
+          ref={inputName}
+        />
+        <input 
+          type='text'
+          placeholder='아이디를 입력하세요'
+          onChange={handleInputId}
+          ref={inputId}
+        />
+        <button type='submit' onClick={handleSubmit}>회원 명부 작성</button>
+      </form>
+      <ul>
+        {
+          userInfo.map((value, index) => (
             <li key={index}>
-              <h3>이름: {value.name}</h3>
-              <strong>아이디값 : {value.id}</strong>
+              <h3>이름 : {value.name}</h3>
+              <strong>아이디 : {value.id}</strong>
             </li>
-          )
-          )
-      }
-    </ul>
-    <span>{getNum(userInfo)}</span>
+          ))
+        }
+      </ul>
+      <span>{getNum(userInfo)}</span>
     </>
   )
 }
-
-// 여기서 부하가 걸리는데 저장할 필요가 있는?없는? 작업은? => getNum
